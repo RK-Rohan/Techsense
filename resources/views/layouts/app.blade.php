13cbc39071e0a8cf2734d796e5ee6ff6e8836da4
Rezaul Karim <rk_rohan@hotmail.com>
2026-05-03 22:22:09 +0600

    chore(ui): set provided logo as site favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -27,6 +27,7 @@
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
         <title>@yield('title') - {{ Session::get('business.name') }}</title>
+        <link rel="icon" type="image/png" href="{{ asset('favicon.png') }}">
         
         @include('layouts.partials.css')
 
